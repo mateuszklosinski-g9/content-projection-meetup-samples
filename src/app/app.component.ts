78e32b17727e232b7f1f9ecdfb523c9c7b0510ed
Mateusz Kłosiński <Mateusz.Klosinski@gravity9.com>
2024-10-05 17:34:51 +0200

    live demo init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherComponent } from './live-demo/before/weather/weather.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WeatherComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'content-projection-meetup-samples';
---
+++ src/app/live-demo/before/weather/weather.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss'],
+  standalone: true,
+})
+export class WeatherComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
